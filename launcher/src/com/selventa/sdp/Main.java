9eeda272a8e7e13a77fcfb2d3253c51d32043eba
Anthony Bargnesi <abargnesi@selventa.com>
2014-01-23 13:36:21 -0500

    updated scy3 launcher; windows cmd exec
    exec .bat file with cmd /c start...
    
    added log writes for debugging purposes

--- launcher/src/com/selventa/sdp/Main.java
+++ launcher/src/com/selventa/sdp/Main.java
@@ -1,21 +1,20 @@
 package com.selventa.sdp;
 
-import static com.selventa.sdp.Util.appendIfMissing;
 import static com.selventa.sdp.Util.equalMD5;
 import static com.selventa.sdp.Util.extract;
 import static com.selventa.sdp.Util.makeExecutable;
 import static com.selventa.sdp.Util.resource;
 import static com.selventa.sdp.Util.windows;
+import static java.lang.String.format;
 import static java.lang.System.getProperty;
 import static java.nio.file.Paths.get;
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Map;
 
 /**
  * Launches cytoscape 3.0.
@@ -75,12 +74,14 @@ public class Main {
      * @throws InterruptedException when this process is interrupted waiting
      * for the Cytoscape process to finish
      */
-    public static void main(String[] args) throws IOException,
-            InterruptedException {
+    public static void main(String[] args) {
 	    File cypath = new File(INSTALL_FOLDER);
 	    if (!cypath.exists()) {
 	        cypath.mkdirs();
 	    }
+
+        final Log log = new Log(new File(cypath, "scy3-launch.log"));
+        log.info(format("Setup installation folder - %s", cypath.getAbsolutePath()));
 
 	    File onDiskZip = new File(cypath, CY_ZIP);
 	    boolean replace = false;
@@ -92,28 +93,74 @@ public class Main {
 	            if (!equalMD5(inJar, onDisk)) {
                     replace = true;
 	            }
-	        }
+            } catch (IOException e) {
+                log.warn("Failure to compare hash of zip file; try to replace", e);
+                replace = true;
+            }
 	    }
 
 	    if (replace) {
+            log.info(format("Update of zip file required - %s", onDiskZip.getAbsolutePath()));
 	        try (InputStream inJar = resource("/" + CY_ZIP)) {
                 Files.copy(inJar, onDiskZip.toPath(), REPLACE_EXISTING);
                 extract(onDiskZip, cypath);
-	        }
-	    }
+                log.info("Successfully updated to latest zip file");
+            } catch (IOException e) {
+                log.fail(format("Failure to replace zip file - %s", onDiskZip.getAbsolutePath()));
+                log.fail("Launch continuing with old cytoscape installation");
+            }
+	    } else {
+            log.info(format("Existing zip file is the latest - %s", onDiskZip.getAbsolutePath()));
+        }
 
         File execFile = get(cypath.getAbsolutePath(), CY_FOLDER,
                 SCRIPT).toFile();
         makeExecutable(execFile);
+        log.dbug(format("Set executable for file - %s", execFile.getAbsolutePath()));
         File karafFile = get(cypath.getAbsolutePath(), CY_FOLDER,
                 KARAF_SCRIPT).toFile();
         makeExecutable(karafFile);
+        log.dbug(format("Set executable for file - %s", karafFile.getAbsolutePath()));
 
-        final Process process = new ProcessBuilder()
-                .command(execFile.getAbsolutePath())
-                .directory(get(cypath.getAbsolutePath(), CY_FOLDER).toFile())
-                .start();
-        int code = process.waitFor();
-        System.exit(code);
-	}
+        File wd = get(cypath.getAbsolutePath(), CY_FOLDER).toFile();
+        final Process process;
+        try {
+            process = makeProcess(wd, execFile, log);
+            log.info("Started process successfully; waiting for process to end");
+            int code = process.waitFor();
+            log.info(format("Process ended, happily? (code %d)", code));
+            System.exit(code);
+        } catch (IOException e) {
+            log.fail(format("Cannot start process; exiting (code 100)"));
+            System.exit(100);
+        } catch (InterruptedException e) {
+            log.fail("Process interrupted", e);
+        }
+    }
+
+    private static Process makeProcess(File workingDirectory, File scriptFile, Log log) throws IOException {
+        final ProcessBuilder bldr = new ProcessBuilder().directory(workingDirectory);
+        if (windows) {
+            bldr.command("cmd", "/c", "start", scriptFile.getAbsolutePath());
+        } else {
+            bldr.command(scriptFile.getAbsolutePath());
+        }
+        log.info(format("Set process command to - %s", Arrays.toString(bldr.command().toArray())));
+
+        if (localJvmPresent(new File(INSTALL_FOLDER))) {
+            String JAVA_HOME = Paths.get(INSTALL_FOLDER, "java_vm").toString();
+            log.info(format("Local JVM exists, courtesy of getdown - %s", JAVA_HOME));
+            Map<String,String> env = bldr.environment();
+            env.put("JAVA_HOME", JAVA_HOME);
+            log.info(format("Set JAVA_HOME to local JVM - %s", JAVA_HOME));
+        } else {
+            log.info("System JVM meets requirements; no local JVM");
+        }
+
+        return bldr.start();
+    }
+
+    private static boolean localJvmPresent(File directory) {
+        return new File(directory, "java_vm").exists();
+    }
 }
